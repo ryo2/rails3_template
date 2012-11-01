432b7de659ab432a1f353fc1e606e92abc2831de
Ryoji Mori <ryo2.dev@gmail.com>
2012-11-01 21:52:42 +0900

    fix method name problem and delete bu files

--- app_template.rb
+++ app_template.rb
@@ -13,13 +13,15 @@ gems = {}
 
 ## Database
 
-if yes?("Would you like to use mysql?")
+gems[:mysql] = yes?("Would you like to use mysql?")
+if gems[:mysql]
   gem 'mysql2'
 end
 
 if yes?("Would you like to use heroku?")
-group :production do
-  gem 'pg'
+  gem_group :production do
+    gem 'pg'
+  end
 end
 
 # soft delete
@@ -82,21 +84,10 @@ run "bundle install"
 remove_file "public/index.html"
 
 # timzone setting
-gsub_file 'config/application.rb', /"# config.time_zone = 'Central Time (US & Canada)'"/, "config.time_zone = 'Tokyo'"
+
+gsub_file "config/application.rb", /# config.time_zone = 'Central Time \(US & Canada\)'/, "config.time_zone = 'Tokyo'"
 
 # database.yml configuration
-@mysql = false
-if yes?("Set up mysql now?")
-  @mysql = {}
-  @mysql[:dev_db] = ask("database name on dev?")
-  @mysql[:dev_un] = ask("user_name on dev?")
-end
-
-if @mysql
-  remove_file "config/database.yml"
-  get "#{repo_url}/config/database.yml", 'config/database.yml'
-  gsub_database 'config/database.yml'
-end
 
 def gsub_database(localpath)
   return unless @mysql
@@ -104,6 +95,21 @@ def gsub_database(localpath)
   gsub_file localpath, /%dev_db%/, @mysql[:dev_db] ? @mysql[:dev_db] : @app_name
   gsub_file localpath, /%dev_un%/, @mysql[:dev_un]
   gsub_file localpath, /%app_name%/, @app_name
+end
+
+if gems[:mysql]
+  @mysql = false
+  if yes?("Set up mysql now?")
+    @mysql = {}
+    @mysql[:dev_db] = ask("database name on dev?")
+    @mysql[:dev_un] = ask("user_name on dev?")
+  end
+end
+
+if @mysql
+  remove_file "config/database.yml"
+  get "#{repo_url}/config/database.yml", 'config/database.yml'
+  gsub_database 'config/database.yml'
 end
 
 # Generators
@@ -128,3 +134,4 @@ end
 git :init
 git :add => '.'
 git :commit => '-am "Initial commit"'
+
